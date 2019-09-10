abf9623e9b7195a53f36893ec696d503b5419d9d
unknown <1527848328@qq.com>
2019-09-10 09:40:37 +0800

    sayhello class

---
+++ sayhello.java
@@ -0,0 +1,16 @@
+public class SayHello{
+    public static void main(Sting[] arge){
+	    System.out.println("Bellow is the function of say hello");
+		SayHello();
+	}
+	public static void SayHello(){
+	    System.out.println("say hello!" + Sum());
+	}
+	public static int Sum(){
+	    int sum=0;
+		for(int i=0;i<=100;i++){
+		    sum = sum + i;
+		}
+		return sum;
+	}
+}
